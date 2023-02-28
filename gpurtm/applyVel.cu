7db19b7ef4403778f42de786fa92ad5332f896d4
吕成照 <109673447+lvchengzhao@users.noreply.github.com>
2023-02-28 11:22:01 +0800

    Update applyVel.cu

--- gpurtm/applyVel.cu
+++ gpurtm/applyVel.cu
@@ -7,17 +7,8 @@
 
 _global_ void applyVel(float* wb,const float *vel,const int nxyz){
     int ix = threadIdx.x+blockIdx.x*blockDim.x;
-    //int iy = threadIdx.y+blockIdx.y*blockDim.y;
-    //int iz = threadIdx.z+blockIdx.z*blockDim.z;
-    //size_t ixz = ix * nz + iz;
-    //size_t nxz = nx * nz;
-    if(ix < nxyz){
+    if(ix < nxyz)
         wb[ix]*=vel[i];
-       // size_t i = iy * nxz+ixz;
-      //  float v2 = vel[i];
-       // float vectx = myLocalWavefield->wb[i];
-       // myLocalWavefield->wb[i] = v2 * vectx;
-    }
 }
 
 
@@ -26,12 +17,13 @@ int main(){
     int nx = myGrid->nx, ny = myGrid->ny, nz = myGrid->nz;
     
     size_t size=nx*ny*nz;
-    size_t nBytes=nxyz*sizeof(float);
+    size_t nBytes=size*sizeof(float);
 
     float *__restrict vel = volModel[VEL];
 
     float *wb = (float*) malloc(nBytes);
-    float *h_wb = (float*) malloc(nBytes);
+    float *gpu_wb = (float*) malloc(nBytes);
+    float *cpu_wb=(float*) malloc(nBytes);
 
     for(int i=0; i<size;i++)
     {
@@ -40,14 +32,14 @@ int main(){
 
     float *d_wb;
     float *d_vel;
-    cudaMalloc((void **)&d_wb, nBytes);
-    cudaMalloc((void **)&d_vel, nBytes);
+    cudaMalloc((void **) &d_wb, nBytes);
+    cudaMalloc((void **) &d_vel, nBytes);
 
-    cudaMemcpy(d_vel,vel,nBytes,cudaMemcpyDefault);
-    cudaMemcpy(d_wb,wb,nBytes,cudaMemcpyDefault);
+    cudaMemcpy(d_vel, vel, nBytes, cudaMemcpyDefault);
+    cudaMemcpy(d_wb, wb, nBytes, cudaMemcpyDefault);
     
-
-
+    
+     
     int dimx = 128;
     dim3 block(dimx);
     dim3 grid((size+block.x-1)/block.x);
@@ -55,6 +47,10 @@ int main(){
     applyVel<<<grid,block>>>(d_wb,d_vel,size);
 
     cudaMemcpy(gpu_wb,d_wb,nBytes,cudaMemcpyDeviceToHost);
+    for(i=0;i<size;i++)
+    cpu_wb[i]=wb[i]*vel[i];
+
+    checkResult(cpu_wb,gpu_wb,size);
 
     cudaFree(d_wb);
     cudaFree(d_vel);
