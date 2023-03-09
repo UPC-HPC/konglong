6e551667fd027e1a770c388a403518fa7fbdc6e2
dinosaur8312 <dinosaur8312@hotmail.com>
2023-03-08 21:09:39 -0600

    add printf for debug

--- gpurtm/Wavefield.cu
+++ gpurtm/Wavefield.cu
@@ -45,40 +45,47 @@ void Wavefield::allocate_mem()
     allocate_gpu_mem();
 }
 
-void Wavefield::allocate_cpu_mem() {
+void Wavefield::allocate_cpu_mem()
+{
+    printf("Start Wavefield::allocate_cpu_mem\n");
 
+    if(!cpuInitialized) 
+    {
+        size_t nxyz = (size_t)nx_ * ny_ * nz_;
+        size_t memSize = nxyz*sizeof(float);
 
-  if(!cpuInitialized) {
-    size_t nxyz = (size_t)nx_ * ny_ * nz_;
-    size_t memSize = nxyz*sizeof(float);
-
-    w0 = (float *)malloc(memSize);
-    w1 = (float *)malloc(memSize);
-    wb = (float *)malloc(memSize);
-    wx = (float *)malloc(memSize);
-    wy = (float *)malloc(memSize);
-    wz = (float *)malloc(memSize);
-    wr = (float *)malloc(memSize);
-  }
+        w0 = (float *)malloc(memSize);
+        w1 = (float *)malloc(memSize);
+        wb = (float *)malloc(memSize);
+        wx = (float *)malloc(memSize);
+        wy = (float *)malloc(memSize);
+        wz = (float *)malloc(memSize);
+        wr = (float *)malloc(memSize);
+    }
     
-  cpuInitialized = true;
+    cpuInitialized = true;
+    printf("End Wavefield::allocate_cpu_mem\n");
 }
-void Wavefield::allocate_gpu_mem() {
+void Wavefield::allocate_gpu_mem() 
+{
 
-  if(!gpuInitialized) {
-    size_t nxyz = (size_t)nx_ * ny_ * nz_;
-    size_t memSize = nxyz*sizeof(float);
+    printf("Start Wavefield::allocate_gpu_mem\n");
+    if(!gpuInitialized) 
+    {
+        size_t nxyz = (size_t)nx_ * ny_ * nz_;
+        size_t memSize = nxyz*sizeof(float);
 
-    checkCudaErrors(cudaMalloc( (void **) &d_w0, memSize));
-    checkCudaErrors(cudaMalloc( (void **) &d_w1, memSize));
-    checkCudaErrors(cudaMalloc( (void **) &d_wb, memSize));
-    checkCudaErrors(cudaMalloc( (void **) &d_wx, memSize));
-    checkCudaErrors(cudaMalloc( (void **) &d_wy, memSize));
-    checkCudaErrors(cudaMalloc( (void **) &d_wz, memSize));
-    checkCudaErrors(cudaMalloc( (void **) &d_wr, memSize));
-  }
+        checkCudaErrors(cudaMalloc( (void **) &d_w0, memSize));
+        checkCudaErrors(cudaMalloc( (void **) &d_w1, memSize));
+        checkCudaErrors(cudaMalloc( (void **) &d_wb, memSize));
+        checkCudaErrors(cudaMalloc( (void **) &d_wx, memSize));
+        checkCudaErrors(cudaMalloc( (void **) &d_wy, memSize));
+        checkCudaErrors(cudaMalloc( (void **) &d_wz, memSize));
+        checkCudaErrors(cudaMalloc( (void **) &d_wr, memSize));
+    }
     
-  gpuInitialized = true;
+    gpuInitialized = true;
+    printf("End Wavefield::allocate_cpu_mem\n");
 }
 
 
@@ -87,8 +94,10 @@ void Wavefield::allocate_gpu_mem() {
  */
 void Wavefield::deallocate_mem() 
 {
+    printf("Start Wavefield::deallocate_mem\n");
     deallocate_cpu_mem();
     deallocate_gpu_mem();
+    printf("End Wavefield::deallocate_mem\n");
     return;
 }
 
@@ -126,7 +135,9 @@ void Wavefield::deallocate_gpu_mem() {
 /*
  * Zero all memory allocated by Wavefield instance (multithreaded)
  */
-void Wavefield::clean_cpu_mem() {
+void Wavefield::clean_cpu_mem()
+{
+    printf("Start Wavefield::clean_cpu_mem\n");
     if(cpuInitialized) 
     {
         size_t nxyz = (size_t)nx_ * ny_ * nz_; 
@@ -139,8 +150,11 @@ void Wavefield::clean_cpu_mem() {
         memset(wz, 0, memSize);
         memset(wr, 0, memSize);
     }
+    printf("End Wavefield::clean_cpu_mem\n");
 }
-void Wavefield::set_data() {
+void Wavefield::set_data() 
+{
+    printf("Start Wavefield::set_data\n");
     if(cpuInitialized) 
     {
         set_data_3d(w0,nx_,ny_,nz_,0);
@@ -157,10 +171,12 @@ void Wavefield::set_data() {
     }
 
     copy_host2dev();
+    printf("End Wavefield::set_data\n");
 }
 
 void Wavefield::copy_host2dev() 
 {
+    printf("Start Wavefield::copy_host2dev\n");
     size_t nxyz = (size_t)nx_ * ny_ * nz_;
     size_t memSize = nxyz*sizeof(float);
     if(cpuInitialized&&gpuInitialized)
@@ -176,13 +192,14 @@ void Wavefield::copy_host2dev()
     }
     else
     {
-    
         printf("ERROR: Either CPU or GPU Memory has not been allocated!\n");
     }
+    printf("End Wavefield::copy_host2dev\n");
 } 
 
 void Wavefield::copy_dev2host() 
 {
+    printf("Start Wavefield::copy_dev2host\n");
     size_t nxyz = (size_t)nx_ * ny_ * nz_;
     size_t memSize = nxyz*sizeof(float);
     if(cpuInitialized&&gpuInitialized)
@@ -197,13 +214,14 @@ void Wavefield::copy_dev2host()
     }
     else
     {
-    
         printf("ERROR: Either CPU or GPU Memory has not been allocated!\n");
     }
+    printf("End Wavefield::copy_dev2host\n");
 } 
 
 int Wavefield::compare_host_dev() 
 {
+    printf("Start Wavefield::compare_host_dev\n");
 
     if( compare_data_3d(w0,d_w0,nx_,ny_,nz_)+
         compare_data_3d(w1,d_w1,nx_,ny_,nz_)+
@@ -218,6 +236,7 @@ int Wavefield::compare_host_dev()
     }
 
     printf("Successful CPU GPU data comparison\n");
+    printf("End Wavefield::compare_host_dev\n");
     return 0;
 
 } 
